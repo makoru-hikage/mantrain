961e95931629e04e43e2497ad9e1979a085caa40
unknown <kurt.ma.coll@gmail.com>
2019-07-12 20:10:53 +0800

    Fix indentations

--- tests/ModuleChainTest.php
+++ tests/ModuleChainTest.php
@@ -8,82 +8,82 @@ use TestDummies\DummyAuthAdapter;
 
 class ModuleChainTest extends TestCase{
 
-	public function testChainSuccess(){
+    public function testChainSuccess(){
 
-		$input = [ "name" => "cmoran", "email" => "cmoran@gmail.com" ];
-		
-		$mantrain = new DummyValidatorModule($input);
+        $input = [ "name" => "cmoran", "email" => "cmoran@gmail.com" ];
 
-        $mantrain = $mantrain
-            ->run(DummyAuthModule::class, ["brando", "contact.create"])
-			->setAdapter(new DummyAuthAdapter)
-            ->run(DummyRandomModule::class)
-            ->run();
-
-		$this->assertEquals(200, $mantrain->getCode());
-		$this->assertEquals(["data" => "YEHEY!"], $mantrain->getData());
-
-	}
-
-	public function testInputWasNotValid(){
-
-		$input = [ "name" => "cmoran", "email" => "cmorangmail.com" ];
-		
-		$mantrain = new DummyValidatorModule($input);
+        $mantrain = new DummyValidatorModule($input);
 
         $mantrain = $mantrain
             ->run(DummyAuthModule::class, ["brando", "contact.create"])
-			->setAdapter(new DummyAuthAdapter)
+            ->setAdapter(new DummyAuthAdapter)
             ->run(DummyRandomModule::class)
             ->run();
 
-		$this->assertEquals(400, $mantrain->getCode());
-		$this->assertEquals(["message" => [ "email" => "Email is invalid" ] ], $mantrain->getData());
+        $this->assertEquals(200, $mantrain->getCode());
+        $this->assertEquals(["data" => "YEHEY!"], $mantrain->getData());
 
-	}
+    }
 
-	public function testNotAllowed(){
+    public function testInputWasNotValid(){
 
-		$input = [ "name" => "cmoran", "email" => "cmoran@gmail.com" ];
-		
-		$mantrain = new DummyValidatorModule($input);
-		$user = "brando";
-		$action = "contact.delete";
+        $input = [ "name" => "cmoran", "email" => "cmorangmail.com" ];
 
-		$mantrain = $mantrain
-			->run(DummyAuthModule::class, [$user, $action])
-			->setAdapter(new DummyAuthAdapter)
+        $mantrain = new DummyValidatorModule($input);
+
+        $mantrain = $mantrain
+            ->run(DummyAuthModule::class, ["brando", "contact.create"])
+            ->setAdapter(new DummyAuthAdapter)
             ->run(DummyRandomModule::class)
             ->run();
 
-		$this->assertEquals(401, $mantrain->getCode());
-		$this->assertEquals(["message" => "Not allowed"], $mantrain->getData());
+        $this->assertEquals(400, $mantrain->getCode());
+        $this->assertEquals(["message" => [ "email" => "Email is invalid" ] ], $mantrain->getData());
 
-	}
+    }
 
-	public function testDislinked(){
+    public function testNotAllowed(){
 
-		$input = [ "name" => "cmoran", "email" => "cmoran@gmail.com" ];
-		
-		$mantrain = new DummyValidatorModule($input);
-		$user = "brando";
-		$action = "contact.delete";
+        $input = [ "name" => "cmoran", "email" => "cmoran@gmail.com" ];
 
-		$validation = $mantrain;
+        $mantrain = new DummyValidatorModule($input);
+        $user = "brando";
+        $action = "contact.delete";
 
-		$authorization = $validation
-			->run(DummyAuthModule::class, [$user, $action])
-			->setAdapter(new DummyAuthAdapter);
-
-
-		$final_step = $authorization
+        $mantrain = $mantrain
+            ->run(DummyAuthModule::class, [$user, $action])
+            ->setAdapter(new DummyAuthAdapter)
             ->run(DummyRandomModule::class)
             ->run();
 
-		$this->assertEquals(401, $final_step->getCode());
-		$this->assertEquals(["message" => "Not allowed"], $final_step->getData());
+        $this->assertEquals(401, $mantrain->getCode());
+        $this->assertEquals(["message" => "Not allowed"], $mantrain->getData());
 
-	}
+    }
+
+    public function testDislinked(){
+
+        $input = [ "name" => "cmoran", "email" => "cmoran@gmail.com" ];
+
+        $mantrain = new DummyValidatorModule($input);
+        $user = "brando";
+        $action = "contact.delete";
+
+        $validation = $mantrain;
+
+        $authorization = $validation
+            ->run(DummyAuthModule::class, [$user, $action])
+            ->setAdapter(new DummyAuthAdapter);
+
+
+        $final_step = $authorization
+            ->run(DummyRandomModule::class)
+            ->run();
+
+        $this->assertEquals(401, $final_step->getCode());
+        $this->assertEquals(["message" => "Not allowed"], $final_step->getData());
+
+    }
 
 
 }
